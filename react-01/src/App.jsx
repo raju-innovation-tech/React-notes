42dfb0eb5d8ba156bd8ba1555e5805c0732e24da
Raju Kumar Maurya <rkmaury860@gmail.com>
2026-03-14 12:50:20 +0530

    react

--- react-01/src/App.jsx
+++ react-01/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="parent">
+        <h2 id="child-1">hello</h2>
+        <p id="child-2" >welcome to my channel</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="uncle">
+        <h2>welcome to my studio</h2>
+        <p>Good Byy</p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+
+    
   )
 }
+
+
 
 export default App
