933efc279538cf26d932f92a4a9a04955e3003c0
Raju Kumar Maurya <rkmaury860@gmail.com>
2026-03-18 14:40:19 +0530

    multiple card create use props

--- 04-cardProject/src/App.jsx
+++ 04-cardProject/src/App.jsx
@@ -1,33 +1,22 @@
 import React from 'react'
-import { Bookmark } from "lucide-react";
+import flipcart from "./assets/flipcart.png";
+import bigbasket from "./assets/big.png";
+import blinkit from "./assets/blinkit.png";
+import swiggy from "./assets/swiggy.png";
+
+import Ca from './components/Card'
 
 const App = () => {
   return (
     <div className="parent">
-      <div className="card">
-        <div className="top">
-          <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" />
-          <button>save <Bookmark size={12} /></button>
-        </div>
-        <div className="center">
-          <div>
-            <h3>Amazon <span>2 Day ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
-            <div className='tag'>
-               <h4>Part Time</h4>
-               <h4>Senior Level</h4>
-             </div>
-          </div>
-        </div>
-        <div className="bottom">
-          <div>
-            <h3>$120/hrs</h3>
-             <p>Noida,India</p>
-          </div>
-          <button>Apply Now</button>
-        </div>
-        
-      </div>
+      
+      <Ca company="Amazon" img="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" />
+      <Ca company="Flipcarts" img={flipcart} />
+      <Ca company="Big-Basket" img={bigbasket} />
+      <Ca company="Blinket" img={blinkit} />
+      <Ca company="Swiggy" img={swiggy} />
+      
+                     
     </div>
   )
 }
