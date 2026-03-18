7f68d6129a996a576660a08debd90649024cff81
Raju Kumar Maurya <rkmaury860@gmail.com>
2026-03-18 12:01:39 +0530

    one card design

--- 04-cardProject/src/App.jsx
+++ 04-cardProject/src/App.jsx
@@ -10,11 +10,13 @@ const App = () => {
           <button>save <Bookmark size={12} /></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>2 Day ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
           <div>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h3>Amazon <span>2 Day ago</span></h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className='tag'>
+               <h4>Part Time</h4>
+               <h4>Senior Level</h4>
+             </div>
           </div>
         </div>
         <div className="bottom">
