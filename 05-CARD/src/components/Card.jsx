e23e45a1676d7379b6cc804ecae636a3aa732429
Raju Kumar Maurya <rkmaury860@gmail.com>
2026-03-18 15:09:15 +0530

    simple method card

---
+++ 05-CARD/src/components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Bookmark } from "lucide-react";
+
+
+const Card = (props) => {
+  return (
+    <div className="card">
+        <div className="top">
+          {/* <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" /> */}
+          <img src={props.img}  />
+          <button>save <Bookmark size={12} /></button>
+        </div>
+        <div className="center">
+          <div>
+            <h3>{props.company} <span>2 Day ago</span></h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className='tag'>
+               <h4>Part Time</h4>
+               <h4>Senior Level</h4>
+             </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hrs</h3>
+             <p>Noida,India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+        
+      </div>
+  )
+}
+
+export default Card
