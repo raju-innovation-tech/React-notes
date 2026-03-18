141fdd72d8eecd0b79b25e612b1e1822b99b5d67
Raju Kumar <rkmaury860@gmail.com>
2026-03-18 17:21:39 +0530

    Card component to use new props

--- 05-CARD/src/components/Card.jsx
+++ 05-CARD/src/components/Card.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+/* import React from "react"; */
 import { Bookmark } from "lucide-react";
 
 
 const Card = (props) => {
+  console.log(props)
   return (
     <div className="card">
         <div className="top">
-          {/* <img src="https://thumbs.dreamstime.com/b/amazon-logo-editorial-illustrative-white-background-eps-download-vector-jpeg-banner-ai-amazon-logo-editorial-illustrative-208329107.jpg" alt="" /> */}
-          <img src={props.img}  />
+          
+          <img src={props.brandLogo}  />
           <button>save <Bookmark size={12} /></button>
         </div>
         <div className="center">
           <div>
-            <h3>{props.company} <span>2 Day ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
+            <h3>{props.companyName} <span>{props.postedDate}</span></h3>
+            <h2>{props.role}</h2>
             <div className='tag'>
-               <h4>Part Time</h4>
-               <h4>Senior Level</h4>
+               <h4>{props.jobType}</h4>
+              <h4>{props.jobLabel} </h4>
              </div>
           </div>
         </div>
         <div className="bottom">
           <div>
-            <h3>$120/hrs</h3>
-             <p>Noida,India</p>
+            <h3>{props.pricePerHour}</h3>
+             <p>{props.location}</p>
           </div>
           <button>Apply Now</button>
         </div>
